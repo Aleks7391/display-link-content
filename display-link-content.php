61be560be63830b6bcf61b001ecb515d916c7e0d
Aleks7391 <aleks.ganev7@gmail.com>
2021-05-31 22:36:52 +0300

    Added correct classes to the 'display' button

--- display-link-content.php
+++ display-link-content.php
@@ -2,38 +2,38 @@
 /**
  * Plugin Name: Display Link Content
  * Description: Upon providing a link, the results will be displayed in the admin menu page.
- * Author:      Aleks Ganev
- * Version:     1.0
+ * Author:	  Aleks Ganev
+ * Version:	 1.0
  */
 
 add_action( 'admin_menu', 'ag_display_link_options_page' );
 
 function ag_display_link_options_page() {
-    add_menu_page(
-        'Display the Link\'s Content',
-        'Link Display Options',
-        'manage_options',
-        'displaylink',
-        'ag_display_link_options_page_html'
-    );
+	add_menu_page(
+		'Display the Link\'s Content',
+		'Link Display Options',
+		'manage_options',
+		'displaylink',
+		'ag_display_link_options_page_html'
+	);
 }
  
  
 function ag_display_link_options_page_html() {
-    ?>
-    <div class="wrap">
-        <h1>Display the Link's Content</h1>
-        <form action="" method="POST" id="display_link_form">
-        <input type="text" name="link_field" id="link_field" value="<?php echo get_transient( 'link' ) ?>" style="width: 700px;">
-	    <br>
-	    <br>
-        <label for="expire_time">Transient expire time in seconds:</label>
-        <input type="text" name="expire_time" id="expire_time" value="60">
-	    <br>
-	    <br>
-        <input type="submit" class="display_link" value="Display">
-        </form>
-    </div>
+	?>
+	<div class="wrap">
+		<h1>Display the Link's Content</h1>
+		<form action="" method="POST" id="display_link_form">
+		<input type="text" name="link_field" id="link_field" value="<?php echo get_transient( 'link' ) ?>" style="width: 700px;">
+		<br>
+		<br>
+		<label for="expire_time">Transient expire time in seconds:</label>
+		<input type="text" name="expire_time" id="expire_time" value="60">
+		<br>
+		<br>
+		<input type="submit" class="display_link button button-primary" value="Display">
+		</form>
+	</div>
 	<br>
 	<div id='content_field' style='contain: content'></div>
 	<?php
@@ -42,26 +42,26 @@ function ag_display_link_options_page_html() {
 add_action("wp_ajax_ag_display_content", "ag_display_content");
 
 function ag_display_content() {
-    $link = $_POST['link'];
-    
-    $expire = intval( absint( $_POST['expire_time'] ));
+	$link = $_POST['link'];
+	
+	$expire = intval( absint( $_POST['expire_time'] ));
 
-    if ( $link == get_transient( 'link' ) ) {
-        $contents = get_transient( 'link_contents' );
+	if ( $link == get_transient( 'link' ) ) {
+		$contents = get_transient( 'link_contents' );
 
-        if ( false === $contents ) {
-            $contents = file_get_contents($link);
-            set_transient( 'link_contents', $contents, $expire );
-        }
+		if ( false === $contents ) {
+			$contents = file_get_contents( $link );
+			set_transient( 'link_contents', $contents, $expire );
+		}
 
-        echo $contents;
-    } else {
-        set_transient( 'link', $link, $expire );
-        $contents = file_get_contents($link);
-        set_transient( 'link_contents', $contents, $expire );
-    
-        echo $contents;
-    }
+		echo $contents;
+	} else {
+		set_transient( 'link', $link, $expire );
+		$contents = file_get_contents( $link );
+		set_transient( 'link_contents', $contents, $expire );
+	
+		echo $contents;
+	}
 	wp_die();
 }
 
@@ -69,7 +69,7 @@ add_action( 'init', 'ag_display_link_script_enqueuer' );
 
 function ag_display_link_script_enqueuer() {
    wp_register_script( "display_link_script", WP_PLUGIN_URL.'/display-link-content/display_link_script.js', array('jquery') );
-   wp_localize_script( 'display_link_script', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));        
+   wp_localize_script( 'display_link_script', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));		
 
    wp_enqueue_script( 'jquery' );
    wp_enqueue_script( 'display_link_script' );
--- display_link_script.js
+++ display_link_script.js
@@ -1,26 +1,26 @@
 jQuery(document).ready(function () {
 
-    jQuery("#display_link_form").submit(function (e) {
-        e.preventDefault();
+	jQuery("#display_link_form").submit(function (e) {
+		e.preventDefault();
 
-        jQuery.ajax({
-            type: "POST",
-            url: myAjax.ajaxurl,
-            data: { action: 'ag_display_content', link: jQuery("#link_field").val(), expire_time: jQuery("#expire_time").val() },
-            success: function (response) {
-                jQuery("#content_field").html(response);
-            }
-        });
-    })
+		jQuery.ajax({
+			type: "POST",
+			url: myAjax.ajaxurl,
+			data: { action: 'ag_display_content', link: jQuery("#link_field").val(), expire_time: jQuery("#expire_time").val() },
+			success: function (response) {
+				jQuery("#content_field").html(response);
+			}
+		});
+	})
 })
 
 jQuery(window).load(function () {
-    jQuery.ajax({
-        type: "POST",
-        url: myAjax.ajaxurl,
-        data: { action: 'ag_display_content', link: jQuery("#link_field").val(), expire_time: 0 },
-        success: function (response) {
-            jQuery("#content_field").html(response);
-        }
-    });
+	jQuery.ajax({
+		type: "POST",
+		url: myAjax.ajaxurl,
+		data: { action: 'ag_display_content', link: jQuery("#link_field").val(), expire_time: 0 },
+		success: function (response) {
+			jQuery("#content_field").html(response);
+		}
+	});
 });
